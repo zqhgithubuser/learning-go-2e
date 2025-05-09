3b33256414f54919cd5e95c7b4e5de4d55c74b2c
zqh1209 <3535521945@qq.com>
2025-05-09 14:59:40 +0800

    Chapter03

--- 03-composite-types/map_read_write/main.go
+++ 03-composite-types/map_read_write/main.go
@@ -3,17 +3,17 @@ package main
 import "fmt"
 
 func main() {
-    totalWins := map[string]int{}
-    totalWins["Orcas"] = 1
-    totalWins["Lions"] = 2
-    fmt.Println(totalWins["Orcas"])   // 1
-    fmt.Println(totalWins["Kittens"]) // 0
+	totalWins := map[string]int{}
+	totalWins["Orcas"] = 1
+	totalWins["Lions"] = 2
+	fmt.Println(totalWins["Orcas"])   // 1
+	fmt.Println(totalWins["Kittens"]) // 0
 
-    totalWins["Kittens"]++
-    fmt.Println(totalWins["Kittens"]) // 1
+	totalWins["Kittens"]++
+	fmt.Println(totalWins["Kittens"]) // 1
 
-    totalWins["Lions"] = 3
-    fmt.Println(totalWins["Lions"]) // 3
+	totalWins["Lions"] = 3
+	fmt.Println(totalWins["Lions"]) // 3
 
-    fmt.Println(totalWins) // map[Kittens:1 Lions:3 Orcas:1]
+	fmt.Println(totalWins) // map[Kittens:1 Lions:3 Orcas:1]
 }
