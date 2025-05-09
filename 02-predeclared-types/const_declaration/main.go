0127debf86e449e1aecb3b45767a85d180f8bc52
zqh1209 <3535521945@qq.com>
2025-05-09 15:03:12 +0800

    Chapter02

--- 02-predeclared-types/const_declaration/main.go
+++ 02-predeclared-types/const_declaration/main.go
@@ -5,15 +5,15 @@ import "fmt"
 const x int64 = 10
 
 const (
-    idKey   = "id"
-    nameKey = "name"
+	idKey   = "id"
+	nameKey = "name"
 )
 
 const z = 20 * 10
 
 func main() {
-    const y = "hello"
+	const y = "hello"
 
-    fmt.Println(x)
-    fmt.Println(y)
+	fmt.Println(x)
+	fmt.Println(y)
 }
--- 02-predeclared-types/type_conversion/main.go
+++ 02-predeclared-types/type_conversion/main.go
@@ -3,14 +3,14 @@ package main
 import "fmt"
 
 func main() {
-    var x = 10
-    var y = 30.2
-    var sum1 = float64(x) + y
-    var sum2 = x + int(y)
-    fmt.Println(sum1, sum2) // 40.2 40
+	var x = 10
+	var y = 30.2
+	var sum1 = float64(x) + y
+	var sum2 = x + int(y)
+	fmt.Println(sum1, sum2) // 40.2 40
 
-    var b byte = 100
-    var sum3 = x + int(b)
-    var sum4 = byte(x) + b
-    fmt.Println(sum3, sum4) // 110 110
+	var b byte = 100
+	var sum3 = x + int(b)
+	var sum4 = byte(x) + b
+	fmt.Println(sum3, sum4) // 110 110
 }
